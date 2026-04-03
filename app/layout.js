00dc2127ac0ce42bd09cfda5b4ca55a405d57977
adelinaismajli007-boop <adelinaismajli007@gmail.com>
2026-04-03 20:49:12 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,14 @@
+export const metadata = {
+  title: 'News Saga',
+  description: 'Latest news from News Saga',
+}
 
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  )
+}
