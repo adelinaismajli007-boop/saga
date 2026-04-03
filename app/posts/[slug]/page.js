6afd9795d3dbe1aadd1c21645d264dc42b62cef1
adelinaismajli007-boop <adelinaismajli007@gmail.com>
2026-04-04 01:19:07 +0200

    Update page.js

--- app/posts/[slug]/page.js
+++ app/posts/[slug]/page.js
@@ -2,7 +2,7 @@ import { redirect } from 'next/navigation';
 
 async function getPost(slug) {
   const res = await fetch(`https://news-saga.com/wp-json/wp/v2/posts?slug=${slug}`, {
-    next: { revalidate: 60 }
+    next: { revalidate: 86400 }
   });
   const posts = await res.json();
   return posts[0];
@@ -12,7 +12,7 @@ export default async function Post({ params }) {
   const post = await getPost(params.slug);
 
   if (!post) {
-    return <div style={{ padding: '20px' }}>Post not found</div>;
+    redirect('https://news-saga.com');
   }
 
   redirect(post.link);
